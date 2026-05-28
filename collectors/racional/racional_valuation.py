0ce722a43adbc5241973f2308388bcbba0bd446d
jm.code <jm.developer.contact@gmail.com>
2026-05-28 13:27:56 -0400

    debugueando

--- collectors/racional/racional_valuation.py
+++ collectors/racional/racional_valuation.py
@@ -35,17 +35,17 @@ def get_racional_valuation():
     ltm_shares = float(os.getenv("RACIONAL_LTM_SHARES", 0))
     meta_shares = float(os.getenv("RACIONAL_META_SHARES", 0))
     amzn_shares = float(os.getenv("RACIONAL_AMZN_SHARES", 0))
-    msft_shares = float(os.getenv("RACIONAL_MSFT_SHARES", 0))
 
     # NUEVO: saldo USD en wallet
     usd_balance = float(os.getenv("RACIONAL_USD_BALANCE", 0))
     print("RAW USD:", repr(usd_balance))
+    if usd_balance == 0:
+        print("error al obtener valor del dolar en variable")
 
     # Precios actuales
     ltm_price = get_price_yahoo_web("LTM.SN")
     meta_price = get_price("META")
     amzn_price = get_price("AMZN")
-    msft_price = get_price("MSFT")
 
     # Valorizaciones
     ltm_value = ltm_shares * ltm_price
@@ -56,15 +56,14 @@ def get_racional_valuation():
     amzn_value = amzn_shares * amzn_price
     amzn_value = usd_to_clp(amzn_value)
 
-    msft_value = msft_shares * msft_price
-    msft_value = usd_to_clp(msft_value)
-
     # NUEVO: convertir dólares de wallet a CLP
     usd_wallet_value = usd_to_clp(usd_balance)
+    if usd_wallet_value == 0:
+        print("error al obtener valor del dolar en funcion a clp")
 
     print(f"Valor dolares: {usd_wallet_value}")
 
-    total = ltm_value + meta_value + amzn_value + msft_value + usd_wallet_value
+    total = ltm_value + meta_value + amzn_value + usd_wallet_value
 
     return {
         "total": total
